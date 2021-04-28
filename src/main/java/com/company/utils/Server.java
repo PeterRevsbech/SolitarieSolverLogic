7f9165f1fd3c75cdabaf3adf2d763c0d2bda7fb2
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2021-04-28 11:44:10 +0200

    started implementing java server

--- src/main/java/com/company/utils/Server.java
+++ src/main/java/com/company/utils/Server.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class Server {
 
@@ -14,7 +15,7 @@ public class Server {
 
     public static void main(String[] args) {
         try {
-            socket = new Socket("localhost", 9999);
+            socket = new Socket("localhost", 9999); //Creates a new socket
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
@@ -24,15 +25,19 @@ public class Server {
             @Override
             public void run() {
                 try {
-                    BufferedReader stdIn =
-                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    while (true) {
-                        String in = stdIn.readLine();
-                        System.out.println(in);
-                        out.print("Message to python from java" + "\r\n");
-                        out.flush();
-                    }
+                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream())); //Gets the list of visible cards from python
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //printwriter is used to send messages back to python
+                    String in = stdIn.readLine(); //saves the input in a string
+                    //System.out.println(in);
+
+                    //Byg et solitaire state ud fra de synlige kort
+
+                    //beregn bedst mulige træk
+
+                    //send trækket til python
+                    out.print("Message to python from java" + "\r\n"); //sends a string to python containing the "best" move
+                    out.flush();
+
 
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
@@ -47,5 +52,12 @@ public class Server {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    //Hjælpe klasse til at returnere moves for at få tingene til at virke
+    public String calcMoves() {
+        int upperBound = 5;
+        String[] str = {"AH -> KS","AH -> KS", "QH -> JS"};
+        return str[new Random().nextInt(upperBound)];
     }
 }
