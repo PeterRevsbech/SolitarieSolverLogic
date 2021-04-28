fb5310ecc34771f79c1f7313b453642f77300f3b
Marie <S185363@student.dtu.dk>
2021-04-28 11:11:58 +0200

    Socket communication

--- src/main/java/com/company/utils/Server.java
+++ src/main/java/com/company/utils/Server.java
@@ -3,13 +3,67 @@ package com.company.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 public class Server {
 
+    static Thread sent;
+    static Thread receive;
+    static Socket socket;
+
+    public static void main(String args[]){
+        try {
+            socket = new Socket("localhost",9999);
+        } catch (UnknownHostException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        sent = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    BufferedReader stdIn =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    while(true){
+                        System.out.println("Trying to read...");
+                        String in = stdIn.readLine();
+                        System.out.println(in);
+                        out.print("Try"+"\r\n");
+                        out.flush();
+                        System.out.println("Message sent");
+                    }
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+        sent.start();
+        try {
+            sent.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+/* // try nr. 1
     public static void main(String[] args) throws IOException {
+
+
+
         String fromclient;
 
         ServerSocket Server = new ServerSocket(5000);
@@ -38,9 +92,11 @@ public class Server {
                 } else {
                     String[] cardArray = fromclient.split(" ");
                     System.out.println("RECIEVED: " + Arrays.toString(cardArray));
-                    
+
                 }
             }
         }
     }
+
+ */
 }
