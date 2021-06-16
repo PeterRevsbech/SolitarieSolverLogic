897f4d3a8d6027e7f26c8f0158855555aaf18026
Marie <S185363@student.dtu.dk>
2021-06-16 12:21:25 +0200

    Tilføjet copyright

--- src/main/java/com/company/models/states/OpenSolitaireState.java
+++ src/main/java/com/company/models/states/OpenSolitaireState.java
@@ -1,3 +1,12 @@
+
+// Gruppe 12 //
+//Christian Kyed - s184210
+//Ida Schrader - s195483
+//Mads Storgaard-Nielsen - s180076
+//Marie Seindal - s185363
+//Peter Revsbech - s183760
+//Sebastian Bjerre - s163526
+
 package com.company.models.states;
 
 import com.company.models.*;
